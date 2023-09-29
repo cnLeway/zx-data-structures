20bbc6803e81644696187b23aa8ab7ede02f5bd8
leway <leway@mydebian.cugb.edu.cn>
2023-09-29 22:18:38 +0800

    leetcode 1

---
+++ 1.两数之和.cpp
@@ -0,0 +1,26 @@
+// No pain, no gain. -- Leway Liu
+// 请在Solution中实现run函数
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
+
+int main(void)
+{
+    Solution mySolution;
+    int rval = mySolution.run();
+    return rval;
+}
